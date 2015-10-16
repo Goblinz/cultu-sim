8a17f9daa88548c25a41810f6e1d00ac25ed2436
Goblinz <goblinz181@gmail.com>
2015-10-16 12:31:46 -0400

    Add the Srum stuff.  Project Back Log is a text file,  Whereas the Srum Back log and the Burn Chart have been merged into a single ods file

---
+++ src/Tile.java
@@ -0,0 +1,8 @@
+
+public class Tile {
+	String name;
+	int x,y;
+	void onMove(Actor actor){ //triggered when an actor move on to it
+		
+	}
+}
--- src/Unit.java
+++ src/Unit.java
@@ -1,4 +1,3 @@
 
 public class Unit extends Actor {
-	
-}
+	}
