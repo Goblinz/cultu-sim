178aca7954171d7c7d85070a534983e8b0829583
JohnRisch <jcrisch@mtu.edu>
2015-10-19 12:36:43 -0400

    added parameters

--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -1,12 +1,37 @@
 package Game;
 
 public abstract class Tile {
+	private String type;
+	private Actor onTile = null;
 	String name;
-	int x,y;
-	void onMove(Actor actor){ //triggered when an actor move on to it
-		
+	private int x,y;
+	
+	public Tile(int X, int Y, String t){
+		x=X;
+		y=Y;
+		type = t;
 	}
+	
+	/**
+	 * when an actor moves onto the tile
+	 * @param actor
+	 */
+	public void onMove(Actor actor){
+		onTile = actor;
+	}
+	
+	public Actor actorOnTile(){ return onTile; }
+	
 	public Resource getResource(){
 		return null;
+	}
+	
+	public int getX(){ return x; }
+	public int getY(){ return y; }
+	
+	public String getType(){ return type; }
+	public void setType(String tp){
+		//TODO check type
+		type = tp;
 	}
 }
