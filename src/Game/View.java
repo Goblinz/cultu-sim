88219a859945c692567a45fc69ece707c316f9ff
JohnRisch <jcrisch@mtu.edu>
2015-10-19 12:44:13 -0400

    added main methods and variables

--- src/Game/View.java
+++ src/Game/View.java
@@ -1,5 +1,34 @@
 package Game;
 
+import java.util.ArrayList;
+
+
 public class View {
 	World gameworld;
+	
+	private Faction currentFaction = null;
+	private Tile currentTile = null;
+	private ArrayList<ArrayList<Tile>> viewedTiles = new ArrayList<ArrayList<Tile>>();
+	private ArrayList<ArrayList<Tile>> allTiles = new ArrayList<ArrayList<Tile>>();
+	private int startX;
+	private int startY;
+	private int endX;
+	private int endY;
+	
+	public View(int sX, int sY, int eX, int eY, ArrayList<ArrayList<Tile>> gameboard){
+		startX = sX;
+		startY = sY;
+		endX = eX;
+		endY = eY;
+		allTiles = gameboard'
+	}
+	
+	public Faction getcurrentFaction(){	return currentFaction;	}
+	public void setCurrentFaction(Faction f){ currentFaction = f; }
+	
+	//sets the tile that is currently being viewed
+	public void setCurrentTile(int x, int y){ currentTile = tiles.get(x).get(y); }
+	public void setCurrentTile(Tile t){ currentTile = t; }
+	
+	public ArrayList<ArrayList<Tile>> getViewedTiles(){ return viewedTiles; }
 }
