3908fed752c628c4101c57a209c3449546a1b339
Goblinz <goblinz181@gmail.com>
2015-10-19 12:32:55 -0400

    Changed the format for the Sprint Back Log to xml so excel should be able to handle that.  Also more incremental Changes

--- src/Game/Actor.java
+++ src/Game/Actor.java
@@ -1,7 +1,11 @@
 package Game;
 
+import java.util.List;
+
 
 public abstract class Actor {
+	private List<Order> possibleOrders;
+	private Order currentOrder;
 	private int posX = 0;
 	private int posY = 0;
 	public static int factionID;
@@ -12,11 +16,14 @@ public abstract class Actor {
 	public void setX(int x){ posX = x; }
 	
 	public void setY(int y){ posY = y; }
-	public void recieveOrder(){
-		
+	public void recieveOrder(Order order){
+		currentOrder = order;
 	}
 	public void act(World world,Faction faction,Actor[][] actors){
 		
+	}
+	public List<Order> PossibleOrder(){
+		return possibleOrders;
 	}
 	
 }
