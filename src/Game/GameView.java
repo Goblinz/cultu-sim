4525b6ad23b245593dba9d965e0f80a5d5b5076b
CareForceOne <mvisconti5@gmail.com>
2015-10-23 13:48:10 -0400

    Created GameView class and updated tile class

---
+++ src/Game/GameView.java
@@ -0,0 +1,80 @@
+package Game;
+
+import javax.swing.JPanel;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class GameView extends JPanel{
+	Tile[][] tiles;
+	final int PAD = 20;
+	final int ROWS = 10;
+	final int COLS = 10;
+	
+	public GameView(){
+		addMouseListener(ml);
+	}
+	
+	protected void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		if(tiles == null){
+			initTiles();
+		}
+		//Draw tiles
+		g2.setPaint(Color.blue);
+		for(int i = 0; i < ROWS; i++){
+			for(int j = 0; j < COLS; j++){
+				tiles[i][j].draw(g2);
+			}
+		}
+	}
+	
+	private void initTiles(){
+		tiles = new Tile[ROWS][COLS];
+		int w = getWidth();
+		int h = getHeight();
+		double xInc = (double)(w - 2 * PAD)/ COLS;
+		double yInc = (double)(h - 2 * PAD)/ ROWS;
+        for(int i = 0; i < ROWS; i++) {
+            double y = PAD + i*yInc;
+            for(int j = 0; j < COLS; j++) {
+                double x = PAD + j*xInc;
+                Rectangle2D.Double r =
+                    new Rectangle2D.Double(x, y, xInc, yInc);
+                tiles[i][j] = new Tile(i, j, r);
+            }
+        }
+	}
+	
+    private MouseListener ml = new MouseAdapter() {
+        public void mousePressed(MouseEvent e) {
+            Point p = e.getPoint();
+            if(!isInGrid(p)) return;
+            double xInc = (double)(getWidth() - 2*PAD)/COLS;
+            double yInc = (double)(getHeight() - 2*PAD)/ROWS;
+            int row = (int)((p.y-PAD)/yInc);
+            int col = (int)((p.x-PAD)/xInc);
+            System.out.println(tiles[row][col]);
+            boolean isSelected = tiles[row][col].isSelected();
+            tiles[row][col].setSelected(!isSelected);
+            repaint();
+        }
+    };
+	
+    private boolean isInGrid(Point p) {
+        Rectangle r = getBounds();
+        r.grow(-PAD, -PAD);
+        return r.contains(p);
+    }
+    
+    private ComponentListener cl = new ComponentAdapter() {
+        public void componentResized(ComponentEvent e) {
+            tiles = null;
+            repaint();
+        }
+    };
+}
--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -1,16 +1,38 @@
 package Game;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
 public class Tile {
+	
+	//matts edit
+	private final int row;
+	private final int col;
+	Rectangle2D.Double rect;
+	Color color = new Color(140,200,160);
+	Color bgColor = Color.orange;
+	Color selColor = Color.red;
+	private boolean selected = false;
+	
+	//nates stuff
 	String name;
 	private String type;
 	private Actor onTile = null;
 	private Resource resource = null;
 	private int x,y;
 	
+	/*
 	public Tile(int X, int Y, String t){
 		x=X;
 		y=Y;
 		type = t;
+	}
+	*/
+	public Tile(int r, int c, Rectangle2D.Double rect){
+		row = r;
+		col = c;
+		this.rect = rect;
 	}
 	
 	/**
@@ -41,4 +63,19 @@ public class Tile {
 		}
 		return true;
 	}
+	
+	//matts edit
+	
+    public void draw(Graphics2D g2) {
+        g2.setPaint(selected ? selColor : bgColor);
+        g2.fill(rect);
+        g2.setPaint(color);
+        g2.draw(rect);
+    }
+    
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+    
+    public boolean isSelected() { return selected; }
 }
