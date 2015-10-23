9650463778f47fc382d498236a5b4117b49ea517
JohnRisch <jcrisch@mtu.edu>
2015-10-22 22:53:09 -0400

    can now change view scope

--- src/Game/View.java
+++ src/Game/View.java
@@ -14,7 +14,16 @@ public class View {
 	private int startY;
 	private int endX;
 	private int endY;
-	
+
+	/**
+	 * input the starting and ending coordinates to be displayed
+	 * 
+	 * @param sX 
+	 * @param sY
+	 * @param eX
+	 * @param eY
+	 * @param world the world object
+	 */
 	public View(int sX, int sY, int eX, int eY, World world){
 		startX = sX;
 		startY = sY;
@@ -25,6 +34,17 @@ public class View {
 		//for now
 		viewedTiles = allTiles;
 	}
+	
+	public void setEnd(int eX, int eY){
+		endX = eX;
+		endY = eY;
+	}
+	
+	public void setStart(int sX, int sY){
+		startX = sX;
+		startY = sY;
+	}
+	
 	
 	public Faction getcurrentFaction(){	return currentFaction;	}
 	public void setCurrentFaction(Faction f){ currentFaction = f; }
