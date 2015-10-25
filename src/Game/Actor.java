150ac1510a2229cb132bc26581f6b99e5907470f
Goblinz <goblinz181@gmail.com>
2015-10-25 14:55:47 -0400

    actors now have held resources

--- src/Game/Actor.java
+++ src/Game/Actor.java
@@ -1,5 +1,7 @@
 package Game;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 
 
@@ -8,7 +10,16 @@ public abstract class Actor {
 	protected int posX = 0;
 	protected int posY = 0;
 	public static int factionID;
-	//public Dictionary  
+	public Dictionary<String,Resource> resources;
+	public Actor(){
+		resources = new Hashtable<String,Resource>();
+		Resource temp = new Resource("Food",0);
+		resources.put(temp.getType(), temp);
+		temp = new Resource("Metal",0);
+		resources.put(temp.getType(), temp);
+		temp = new Resource("Wood",0);
+		resources.put(temp.getType(), temp);
+	}
 	public int getX(){ return posX; }
 	
 	public int getY(){ return posY; }
