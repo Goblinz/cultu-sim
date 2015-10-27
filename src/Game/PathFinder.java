8b692beff886c5591b43833754dc8a1942b7f7a4
JohnRisch <jcrisch@mtu.edu>
2015-10-27 17:02:50 -0400

    added getPathMethod and vars

--- src/Game/PathFinder.java
+++ src/Game/PathFinder.java
@@ -1,5 +1,31 @@
 package Game;
+import java.awt.Point;
+import java.util.ArrayList;
 
 public class PathFinder {
 
+	Actor actor;
+	World world;
+	Point toPoint;
+	ArrayList<Tile> path;
+	
+	
+	public PathFinder(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param w the world object
+	 * @param a the actor
+	 * @param p the point to move to
+	 * @return an array of Tiles that is the shortest not obstructed path
+	 */
+	public Tile[] getPathTo(World w, Actor a, Point p){
+		actor = a;
+		world = w;
+		toPoint = p;
+		return null;
+	}
+	
 }
