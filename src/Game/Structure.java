2530145776d829276aa12225f6e07e4fe486ce4a
John Risch <jcrisch@c28-0117-15.ad.mtu.edu>
2015-11-09 13:47:16 -0500

    PathFinder added to MoveGather

--- src/Game/Structure.java
+++ src/Game/Structure.java
@@ -2,19 +2,29 @@ package Game;
 
 import java.awt.Color;
 
-
 public class Structure extends Actor {
-	public Structure(int factionid,int id, int x,int y,World world){
-		super(x,y,world,id,factionid,ActorType.STRUCTURE);
+	public Structure(int factionid, int id, int x, int y, World world) {
+		super(x, y, world, id, factionid, ActorType.STRUCTURE);
 	}
-	public void act(World world,Faction faction){
+
+	public void act(World world, Faction faction) {
 		Tile onTile = world.getTiles()[posX][posY];
-		if(onTile.getType() == TileType.FERTILELAND)
-			resources.get("Food").addQuantity(5);
-		else if(onTile.getType() == TileType.MINE)
-			resources.get("Metal").addQuantity(5);
-		else if(onTile.getType() == TileType.FOREST)
-			resources.get("Wood").addQuantity(5);
+		if (onTile.getType() == TileType.FERTILELAND) {
+			if(faction.techTree.hasTech("Grain Storage"))
+				resources.get("Food").addQuantity(7);
+			else
+				resources.get("Food").addQuantity(5);
+		} else if (onTile.getType() == TileType.MINE) {
+			if(faction.techTree.hasTech("Shaft Mining"))
+				resources.get("Metal").addQuantity(7);
+			else
+				resources.get("Metal").addQuantity(5);
+		} else if (onTile.getType() == TileType.FOREST) {
+			if(faction.techTree.hasTech("Axes"))
+				resources.get("Wood").addQuantity(7);
+			else
+				resources.get("Wood").addQuantity(5);
+		}
 	}
 
 }
